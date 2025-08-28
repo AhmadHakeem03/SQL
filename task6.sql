5bfb790188385fb3a025170970daa041d40fdc34
Ahmad Saleh <137359251+AhmadHakeem03@users.noreply.github.com>
2025-08-28 12:28:05 +0300

    Delete task6.sql

--- task6.sql
+++
@@ -1,5 +0,0 @@
--- Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
-
-SELECT  gender, category, count(transactions_id)
-from retail_sales
-GROUP BY category, gender
