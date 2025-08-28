2b6406b0cf4467f170a8e5a1a092014cc84462db
Ahmad Saleh <137359251+AhmadHakeem03@users.noreply.github.com>
2025-08-28 12:27:57 +0300

    Delete task5.sql

--- task5.sql
+++
@@ -1,5 +0,0 @@
--- Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
-
-SELECT AVG(age) AS average_age
-FROM retail_sales
-WHERE category = 'Beauty';
